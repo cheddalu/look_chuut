aa44d21445a9c11d70eb9a180a71323131067913
cheddalu <githubcheddalu@retsehc.xyz>
2020-04-17 11:51:31 -0700

    fixed language

--- bmi.py
+++ bmi.py
@@ -1,21 +1,21 @@
-# weight = int(input("How many lbs are ya? "))
-# height = int(input("Give me your height in inches: "))
-weight = 180.5
-height = 70
+weight = int(input("How many lbs are ya? "))
+height = int(input("Give me your height in inches: "))
 
+# weight = 270
+# height = 70
 # print(type(weight))
 # print(type(height))
 
 def bmi_calculator(weight, height):
     bmi = round((weight * 703) / (height ** 2),2)
-    print("bmi: " + str(bmi))
+    print("Your bmi is: " + str(bmi))
     if (bmi < 18.5): 
-        print('You is underweight')
+        print('You\'re a bit underweight')
     elif (bmi >= 18.5 and bmi <= 24.9):
-        print('You\'re normal')
+        print('Congratulations you\'re normal')
     elif (bmi >= 25 and bmi <= 29.9):
-        print('You\'re overweight')
+        print('You\'re a bit overweight')
     else: 
-        print('You obese!!!')
+        print('You\'re considered obese. Start getting more active.')
 
 bmi_calculator(weight, height)
