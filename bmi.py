7eb719df68cb5eaedd9e7e511ff5a2f6f60611e6
cheddalu <githubcheddalu@retsehc.xyz>
2020-04-17 11:46:08 -0700

    removed clutter

--- bmi.py
+++ bmi.py
@@ -1,25 +1,12 @@
 # weight = int(input("How many lbs are ya? "))
 # height = int(input("Give me your height in inches: "))
-
-# bmi_weight = (weight * 703)
-# bmi_height = (height ** 2)
-
-# bmi = round((bmi_weight / bmi_height),2)
-
-# print(f'Your BMI is {bmi}')
-
 weight = 180.5
-
 height = 70
+
 # print(type(weight))
 # print(type(height))
 
 def bmi_calculator(weight, height):
-    # print(weight)
-    # print(height)
-    # htsq = (height ** 2)
-    # print(htsq)
-
     bmi = round((weight * 703) / (height ** 2),2)
     print("bmi: " + str(bmi))
     if (bmi < 18.5): 
@@ -32,24 +19,3 @@ def bmi_calculator(weight, height):
         print('You obese!!!')
 
 bmi_calculator(weight, height)
-
-
-
-# bmi = 18
-# if bmi < 18.5: 
-#     print('Underweight')
-# else: 
-#     if bmi > 30.0:
-#         print('Obesly fat')
-    # elif bmi in range(18.5,24.9):
-    #     print('Normal')
-    # elif bmi in range(25,29.9):
-    #     print('Overweight')
-
-
-# def bmifunction(x):
-#     return 2*x
-
-# a = bmifunction(3)
-
-# print(a)
