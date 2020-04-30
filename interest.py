b1928cedbb6538699660120096f8b1ee92f88dfb
cheddalu <githubcheddalu@retsehc.xyz>
2020-04-29 22:30:58 -0700

    interest calculator

---
+++ interest.py
@@ -0,0 +1,7 @@
+p = int(input('Enter principal value for p: '))
+n = int(input('Enter number of years value for n: '))
+r = float(input('Enter rate value for r: '))
+# print(f'{p}, {n}, {r}')
+
+si = ((p * n * r)/100)
+print(si)
