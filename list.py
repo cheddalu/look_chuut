d81773f1b3cecc7a6c2545f4f20786b4222e8f6a
cheddalu <githubcheddalu@retsehc.xyz>
2020-05-03 23:47:32 -0700

    list

---
+++ list.py
@@ -0,0 +1,11 @@
+'''List out all the odd numbers from 1 to 100 using lists in Python.
+'''
+
+#Right way to do it:
+# number_list = list(range(1,101))
+# for x in number_list:
+#     if x %2 != 0:
+#         print(x)
+
+#Cheating way to do it
+# print(list(range(1,101, 2)))
